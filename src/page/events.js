c9c9060504f0eb57365a2c0ca90cdb64fb8d340c
fearofcod0001 <dlxo8888@gmail.com>
2023-09-08 15:29:34 +0900

    이벤트 추가

---
+++ src/page/events.js
@@ -0,0 +1,17 @@
+const now = new Date();
+
+export default [
+  {
+    id: 0,
+    title: "All Day Event very long title",
+    allDay: true,
+    start: new Date("2023, 9, 8"),
+    end: new Date("2023, 9, 8"),
+  },
+  {
+    id: 1,
+    title: "Long Event",
+    start: new Date("2023-9-9"),
+    end: new Date("2023-9-9"),
+  },
+];
